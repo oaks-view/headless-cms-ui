10ebc85f6abeb83052098cf195507ee4fc4f518c
Moses igbuku <captainoak@gmail.com>
2020-02-11 13:09:18 +0100

    intialised project

---
+++ pages/Index.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import '../styles/main.scss';
+
+export default function Index() {
+  return (
+    <Layout>
+      <p>LetsCoOwn Home</p>
+    </Layout>
+  );
+}
---
+++ pages/Strapi.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import '../styles/main.scss';
+
+export default function Strapi() {
+  return (
+    <Layout>
+      <p>Strapi client page</p>
+    </Layout>
+  );
+}
