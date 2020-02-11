e2197e100563ac4f592c5d56cb0a19acb6add7ce
Moses igbuku <captainoak@gmail.com>
2020-02-11 13:49:12 +0100

    updated nav bar

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,47 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 
 const linkStyle = {
-  marginRight: 15
+  marginRight: 15,
+  paddingRight: 1,
+  paddingLeft: 1
 };
 
-const Header = () => (
-  <div>
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>About</a>
-    </Link>
-    <Link href="/strapi">
-      <a style={linkStyle}>Strapi</a>
-    </Link>
-  </div>
-);
+const selectedLinkStyle = {
+  borderColor: "red",
+  backgroundColor: "#B095C9",
+  borderRadius: 3,
+  textDecoration: "none",
+  color: "#ffff",
+  fontSize: "1.1em",
+  minWidth: "4em",
+  display: "inline-block",
+  textAlign: "center",
+  fontWeight: "600"
+};
+
+const PAGES = {
+  HOME: '/',
+  ABOUT: "/about",
+  STRAPI: "/strapi"
+};
+
+const Header = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <Link href="/">
+        <a style={{...linkStyle, ...(router.pathname === PAGES.HOME && selectedLinkStyle || {})}}>Home</a>
+      </Link>
+      <Link href={PAGES.ABOUT}>
+        <a style={{...linkStyle, ...(router.pathname === PAGES.ABOUT && selectedLinkStyle || {})}}>About</a>
+      </Link>
+      <Link href={PAGES.STRAPI}>
+        <a style={{...linkStyle, ...(router.pathname === PAGES.STRAPI && selectedLinkStyle || {})}}>Strapi</a>
+      </Link>
+    </div>
+  )
+};
 
 export default Header;
--- pages/Strapi.js
+++ pages/Strapi.js
@@ -2,9 +2,10 @@ import Layout from '../components/Layout';
 import '../styles/main.scss';
 
 export default function Strapi() {
-  return (
-    <Layout>
-      <p>Strapi client page</p>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <h2>Strapi client page</h2>
+            <p>This screen just fetches blog entries from strapi api's and displays them</p>
+        </Layout>
+    );
 }
