c93f43b45c299e61292a24b78eadd69a99b14de9
Moses igbuku <captainoak@gmail.com>
2020-02-11 16:44:58 +0100

    removed a style

--- pages/Strapi/[id]/index.js
+++ pages/Strapi/[id]/index.js
@@ -3,14 +3,23 @@ import Layout from '../../../components/Layout';
 import StrapiService from '../../../services/StrapiService';
 
 const styles = {
-
+    container: {
+        color: 'red',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    blogHeading: {
+        color: "#ffff",
+        backgroundColor: '#023879'
+    }
 };
 
 function Strapi({ blog }) {
     return (
         <Layout>
             <h2>Strapi Blog detail page</h2>
-            <h4 style={{ color: "#B095C9" }}>{blog.title}</h4>
+            <h4 style={styles.blogHeading}>{blog.title}</h4>
 
             <div>
                 {/* {blog.body} */}
