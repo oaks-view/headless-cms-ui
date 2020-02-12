fd47523d24ccb219a9b68ca3dac28e9b5cba8c92
Moses igbuku <captainoak@gmail.com>
2020-02-12 14:59:32 +0100

    added book detail screen

--- pages/Contentful/[id]/index.js
+++ pages/Contentful/[id]/index.js
@@ -2,6 +2,7 @@ import ReactMarkdown from "react-markdown";
 import LayoutContentful from '@components/LayoutContentful';
 import ContentfulService from '@services/ContentfulService';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import * as _ from 'lodash';
 
 const styles = {
     container: {
@@ -13,16 +14,33 @@ const styles = {
     bookTitle: {
         color: "#ffff",
         backgroundColor: '#023879'
+    },
+    bookCover: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        width: '60%',
+        minheight: '40rem'
+    },
+    coverPhoto: {
+        width: '30%',
+        marginRight: '1.2rem',
+    },
+    firstPage: {
+        width: '65%',
     }
 };
 
 function ContentfulBookDetailPage({ book }) {
+    const coverPhotoUrl = _.get(book, 'fields.coverPhoto.fields.file.url');
     return (
         <>
             <h2>Contentful Book detail page</h2>
             <h4 style={styles.bookTitle}>{book.fields.title}, by - {book.fields.author}</h4>
-            <div>
-                {documentToReactComponents(book.fields.firstPage)}
+            <div style={styles.bookCover}>
+                <img src={coverPhotoUrl} alt='image not found' height={90} width={50}/>
+                <div style={styles.firstPage}>{documentToReactComponents(book.fields.firstPage)}</div>
             </div>
         </>
     );
